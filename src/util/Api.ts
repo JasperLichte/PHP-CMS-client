dfaf36920c67d1b6057531963de4a553de5faf04
Jasper Lichte <jlichte@hh.rexx-systems.rx>
2020-04-09 14:28:19 +0200

    Refactor Api

--- src/util/Api.ts
+++ src/util/Api.ts
@@ -1,10 +1,11 @@
 import ApiResponse from "./api/ApiResponse";
+import RequestFactory from "./api/RequestFactory";
 
 const base_url = 'http://localhost:8073/Projects/PHP-CMS/api'; // TODO move in config file
 
 class Api {
 
-    public static get(path: string, license: string): Promise<ApiResponse<any>> {
+    private static get(path: string, license: string): Promise<ApiResponse<any>> {
         return new Promise(async (resolve, reject) => {
             const url = new URL(`${base_url}${path}`);
             url.searchParams.delete('license');
@@ -16,7 +17,7 @@ class Api {
         });
     }
 
-    public static post(path: string, license: string) {
+    private static post(path: string, license: string) {
         return (body: {}) => {
             return new Promise(async (resolve, reject) => {
                 const json = (await fetch(
@@ -29,6 +30,18 @@ class Api {
                 resolve((new ApiResponse(json)).deserialize(json));
             });
         }
+    }
+
+    public static async makeGetRequest<T>(obj: T, url: string): Promise<ApiResponse<T>> {
+        const res = await Api.get(url, RequestFactory.license);
+        // @ts-ignore
+        return (new ApiResponse(obj.deserialize(res.data.data))).deserialize(res);
+    }
+
+    public static async makePostRequest<T>(obj: T, url: string, body: {}): Promise<ApiResponse<T>> {
+        const res = await Api.post(url, RequestFactory.license)(body);
+        // @ts-ignore
+        return (new ApiResponse(obj.deserialize(res.data.data))).deserialize(res);
     }
 
 }
--- src/util/api/RequestFactory.ts
+++ src/util/api/RequestFactory.ts
@@ -1,34 +1,21 @@
-import ApiResponse from "./ApiResponse";
 import Api from "../Api";
 import PreflightResponse from "./responses/PreflightResponse";
 import GetPageResponse from "./responses/GetPageResponse";
 
 export default class RequestFactory {
 
-    private static license: string = '';
-
-    private static async makeGetRequest<T>(obj: T, url: string): Promise<ApiResponse<T>> {
-        const res = await Api.get(url, RequestFactory.license);
-        // @ts-ignore
-        return (new ApiResponse(obj.deserialize(res.data.data))).deserialize(res);
-    }
-
-    private static async makePostRequest<T>(obj: T, url: string, body: {}): Promise<ApiResponse<T>> {
-        const res = await Api.post(url, RequestFactory.license)(body);
-        // @ts-ignore
-        return (new ApiResponse(obj.deserialize(res.data.data))).deserialize(res);
-    }
+    public static license: string = '';
 
     public static async preflight(license: string) {
         RequestFactory.license = license;
-        return RequestFactory.makeGetRequest<PreflightResponse>(
+        return Api.makeGetRequest<PreflightResponse>(
             new PreflightResponse(),
             `/preflight.php`
         );
     }
 
     public static async getPage(slug: string) {
-        return RequestFactory.makeGetRequest<GetPageResponse>(
+        return Api.makeGetRequest<GetPageResponse>(
             new GetPageResponse(),
             `/page/get.php?p=${slug}`
         );
