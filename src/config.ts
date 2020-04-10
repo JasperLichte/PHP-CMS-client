5e0f9616c68664b3c7e5c6d646bb09e0fbbda715
Jasper Lichte <jlichte@hh.rexx-systems.rx>
2020-04-10 13:30:36 +0200

    Deploy to github pages

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export default {
-    PRODUCTION: false,
+    PRODUCTION: true,
     LICENSE: '123',
-    BASE_PROD_API_URL: 'http://jaspress.lichte.info/api',
+    BASE_PROD_API_URL: 'https://lichte.info/jaspress/api',
     BASE_DEV_API_URL: 'http://localhost:8073/Projects/PHP-CMS/api',
 };
