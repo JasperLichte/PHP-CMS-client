01edb95199c1eb2c517939cedeea23d579ac1fef
Jasper Lichte <jlichte@hh.rexx-systems.rx>
2020-04-10 12:27:51 +0200

    Updates

---
+++ precache-manifest.fa7a40654db15d9e0e1f489ae786bb8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad39b9f8b56cc2c21033616e59f01abc",
+    "url": "/jaspress-client/index.html"
+  },
+  {
+    "revision": "d859e670e01f64437029",
+    "url": "/jaspress-client/static/css/main.7dcc525d.chunk.css"
+  },
+  {
+    "revision": "fb4b2022ef7c54d1c49d",
+    "url": "/jaspress-client/static/js/2.89657cbc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jaspress-client/static/js/2.89657cbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d859e670e01f64437029",
+    "url": "/jaspress-client/static/js/main.08d261ea.chunk.js"
+  },
+  {
+    "revision": "f65a2f09f23bb45463c4",
+    "url": "/jaspress-client/static/js/runtime-main.24af1705.js"
+  }
+]);
