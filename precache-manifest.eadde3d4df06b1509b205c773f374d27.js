c833ddee636c78023542287c84f23c88768caeb5
Jasper Lichte <jlichte@hh.rexx-systems.rx>
2020-04-17 10:36:54 +0200

    Updates

---
+++ precache-manifest.eadde3d4df06b1509b205c773f374d27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac6558830f35ee13e33133fafdc5f552",
+    "url": "/jaspress-client/index.html"
+  },
+  {
+    "revision": "7ab3a606b873634b922b",
+    "url": "/jaspress-client/static/css/main.01d487ca.chunk.css"
+  },
+  {
+    "revision": "1cc360fdca4289e5bad9",
+    "url": "/jaspress-client/static/js/2.92372f2b.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/jaspress-client/static/js/2.92372f2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ab3a606b873634b922b",
+    "url": "/jaspress-client/static/js/main.34d2f4c7.chunk.js"
+  },
+  {
+    "revision": "f65a2f09f23bb45463c4",
+    "url": "/jaspress-client/static/js/runtime-main.24af1705.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jaspress-client/precache-manifest.6f087f7f2c1f2d9857419ca903a4fff7.js"
+  "/jaspress-client/precache-manifest.eadde3d4df06b1509b205c773f374d27.js"
 );
 
 self.addEventListener('message', (event) => {
